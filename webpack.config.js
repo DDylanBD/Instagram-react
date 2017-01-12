bfbd09643edaf85884639c999b9b292c01c33a08
Admin <beaucamps.dylan@gmail.com>
2017-01-12 13:41:42 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+	entry: APP_DIR + '/index.jsx',
+	output: {
+		path: BUILD_DIR,
+		filename: 'bundle.js'
+	},
+	module: {
+   loaders: [{
+     test: /\.scss$/,
+     loaders: ["style-loader", "css-loader", "sass-loader"]
+   }, {
+     test: /\.jsx$/,
+     loader: "babel-loader",
+     query: {
+       presets: ['es2015', 'react']
+     }
+   }, {
+     test: /\.(jpg|png)$/,
+     loader: 'url-loader',
+     options: {
+       limit: 25000
+     }
+   }, {
+     test: /\.(jpg|png|otf)$/,
+     loader: 'file-loader',
+   },
+   {
+     test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+     loader: "url-loader?limit=10000&mimetype=application/font-woff"
+   },
+   {
+     test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+     loader: "file-loader"
+   }]
+ }
+};
+
+module.exports = config;
